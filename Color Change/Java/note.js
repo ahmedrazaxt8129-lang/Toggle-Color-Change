9fd0564e9009534c2918e34b3474f32a81aa90bb
Ahmed Raza <ahmedrazaxt8129@gmail.com>
2026-09-13 08:28:20 -0700

    Clean up note.js by removing commented code
    Removed commented-out code for the toggleColor function.

--- Color Change/Java/note.js
+++ Color Change/Java/note.js
@@ -1,38 +1,3 @@
-// function toggleColor (){
-// var image = document.getElementById("image");
-// image.setAttribute("class", "red");
-// }
-
-//  function toggleColor() {
-//     var image = document.getElementById("image");
-
-//     if (image.classList.contains("green")) {
-//       image.setAttribute("class", "red");
-//     } else {
-//       image.setAttribute("class", "green");
-//     }
-//   }
-
-  // var image = document.getElementById('image');
-
-  //   if (image.className == 'green') {
-  //     image.setAttribute('class', 'red');
-  //   } else {
-  //     image.setAttribute('class', 'green');
-  //   }
-
-//  function toggleColor() {
-//       var image = document.getElementById("image");
-
-//       if (image.getAttribute("class") == "green") {
-//         image.setAttribute("class", "red");
-//       }
-
-//       if (image.getAttribute("class") == "red") {
-//         image.setAttribute("class", "green");
-//       }
-//     }
-
 function toggleColor() {
     var image = document.getElementById("image");
 
